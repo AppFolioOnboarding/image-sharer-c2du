cf7063c70131bbe325e37238a4c63ee68d546977
Connie Du <connie.du@appfolio.com>
2019-04-23 16:10:22 -0700

    cd - add image seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
+# rubocop:disable Metrics/LineLength
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+image_list = [
+  { url: 'http://sfwallpaper.com/images/pretty-nature-wallpaper-1.jpg' },
+  { url: 'https://i.pinimg.com/originals/e9/2c/9a/e92c9a191361bf8e6d71cf014ce69e75.jpg' },
+  { url: 'http://3.bp.blogspot.com/-uN11QTqFuwU/TnJqVvMhLBI/AAAAAAAAFJM/r14eFUbh4Eo/s1600/pretty+nature+pics-1.jpg' },
+  { url: 'https://i.imgur.com/avgBnhW.jpg' },
+  { url: 'https://wallpapercave.com/wp/eyPmGyU.jpg' },
+  { url: 'http://doanarae.com/doanarae/7813-pretty-nature-wallpapers_7355.jpg' },
+  { url: 'https://i.pinimg.com/originals/e9/9f/29/e99f295611ec28563096846c20199593.jpg' },
+  { url: 'https://data.whicdn.com/images/22762914/original.jpg' },
+  { url: 'https://cdn.iwastesomuchtime.com/97201220290525MostBeautifulAnimalsPhotographyStumbleUpon3.jpg' },
+  { url: 'https://i.pinimg.com/originals/0e/32/a8/0e32a8ba6249b7c5f7bad8858eb3cbab.jpg' },
+  { url: 'https://i.ytimg.com/vi/L2WgJgAULo8/maxresdefault.jpg' },
+  { url: 'http://www.fulldose.net/wp-content/uploads/2015/12/So-Pretty-Nature.jpg' },
+  { url: 'https://sunpathfinancial.com/wordpress/wp-content/uploads/2018/05/pretty-nature-pictures-beautiful-nature-background-pictures.jpg' },
+  { url: 'http://sfwallpaper.com/images/beautiful-hd-wallpapers-for-desktop-30.jpg' },
+  { url: 'https://www.wallpapersbrowse.com/images/ro/roaxr44.jpg' },
+  { url: 'http://vuedesign.net/wp-content/uploads/pretty-nature-pics-pretty-pictures-of-nature-pretty-nature-muselove28-on.jpg' },
+  { url: 'https://thewallpaper.co/wp-content/uploads/2016/06/pretty-nature-hd-wallpaper-desktop-background-f-download-wallpaper-desktop-images-mac-desktop-images-1920x1200-768x480.jpg' },
+  { url: 'https://data.whicdn.com/images/158083559/large.jpg' },
+  { url: 'https://assets3.thrillist.com/v1/image/1549228/size/tmg-facebook_social.jpg' },
+  { url: 'http://www.atbreak.com/wp-content/uploads/2011/12/damn-nature-u-pretty.jpg' }
+]
+
+Image.create!(image_list)
+# rubocop:enable Metrics/LineLength
