acbf8f1199166ec189401850f88203072bce301b
Connie Du <connie.du@appfolio.com>
2019-04-25 13:16:44 -0700

    cd/de - add feedback page with name field and comments field form and footer

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import Header from './Header';
 
 export default function App() {
   return (
     <div>
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Form>
+        <FormGroup>
+          <Label for="exampleName">Your name:</Label>
+          <Input type="name" name="name" id="exampleName" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleComments">Comments:</Label>
+          <Input type="textarea" name="comments" id="exampleComments" />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+      <footer>
+        Copyright: Appfolio Inc. Onboarding
+      </footer>
     </div>
   );
 }
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -9,5 +9,9 @@ describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
+    assert.strictEqual(wrapper.find('input[name="name"]').length, 1);
+    assert.strictEqual(wrapper.find('textarea[name="comments"]').length, 1);
+    assert.strictEqual(wrapper.find('button').text(), 'Submit');
+    assert.strictEqual(wrapper.find('footer').text(), 'Copyright: Appfolio Inc. Onboarding');
   });
 });
