b2092ef1fefccfc4506a0659c72b99262acc5d62
Connie Du <connie.du@appfolio.com>
2019-04-29 11:29:40 -0700

    cd/ew/fy/ds - submit feedback to backend, show alert displaying status of response, clear form on submit

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -2,16 +2,76 @@
 
 import assert from 'assert';
 import { mount } from 'enzyme';
+import sinon from 'sinon';
 import React from 'react';
 import App from '../../components/App';
+import * as api from '../../utils/helper';
 
 describe('<App />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+
   it('should render correctly', () => {
-    const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
     assert.strictEqual(wrapper.find('input[name="name"]').length, 1);
     assert.strictEqual(wrapper.find('textarea[name="comments"]').length, 1);
     assert.strictEqual(wrapper.find('button').text(), 'Submit');
     assert.strictEqual(wrapper.find('footer').text(), 'Copyright: Appfolio Inc. Onboarding');
+  });
+
+  describe('submitFeedback', () => {
+    const sandbox = sinon.createSandbox();
+    let preventDefaultSpy;
+
+    beforeEach(() => {
+      preventDefaultSpy = sandbox.spy();
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('should send correct api call', () => {
+      const postStub = sandbox.stub(api, 'post').resolves({ status: 'fakeStatus', message: 'fakeMessage' });
+      wrapper.instance().setState({ name: 'Conn', comments: 'whatever' });
+      wrapper.update();
+      return wrapper.instance().submitFeedback({ preventDefault: preventDefaultSpy }).then(() => {
+        assert(postStub.calledOnceWithExactly('/api/feedbacks', { feedback: { name: 'Conn', comments: 'whatever' } }));
+      });
+    });
+
+    it('api call should fail with empty form', () => {
+      const postStub = sandbox.stub(api, 'post').rejects({ data: { status: 'fakeStatus', message: 'fakeMessage' } });
+      wrapper.instance().setState({ name: 'Conn', comments: 'whatever' });
+      wrapper.update();
+      return wrapper.instance().submitFeedback({ preventDefault: preventDefaultSpy }).then(() => {
+        assert(postStub.calledOnceWithExactly('/api/feedbacks', { feedback: { name: 'Conn', comments: 'whatever' } }));
+      });
+    });
+
+    it('should render alert based on status and message and clear form', () => {
+      const postStub = sandbox.stub(api, 'post').resolves({ status: 'fakeStatus', message: 'fakeMessage' });
+      wrapper.instance().setState({ name: 'Conn', comments: 'whatever' });
+      wrapper.update();
+      return wrapper.instance().submitFeedback({ preventDefault: preventDefaultSpy }).then(() => {
+        assert(postStub.calledOnceWithExactly('/api/feedbacks', { feedback: { name: 'Conn', comments: 'whatever' } }));
+        wrapper.update();
+        assert.strictEqual(wrapper.find('div.alert-fakeStatus').length, 1);
+        assert.strictEqual(wrapper.find('div.alert-fakeStatus').text(), 'fakeMessage');
+        assert.strictEqual(wrapper.find('input[name="name"]').text(), '');
+        assert.strictEqual(wrapper.find('textarea[name="comments"]').text(), '');
+      });
+    });
+
+    it('preventDefault should be called once', () => {
+      sandbox.stub(api, 'post').resolves();
+      const event = { preventDefault: preventDefaultSpy };
+      return wrapper.instance().submitFeedback(event).then(() => {
+        assert(preventDefaultSpy.calledOnce);
+      });
+    });
   });
 });
